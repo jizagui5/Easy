a6a17ad34c11e6474db075a6ea6bc81d8d64cfc5
jizagui5 <javi@Javiers-MacBook.local>
2016-10-04 12:51:19 -0500

    Codeeval Odd Numbers

---
+++ Main.java
@@ -0,0 +1,16 @@
+ /// Print odd numbers from one to zero
+///Print the odd numbers from 1 to 99, one number per line. 
+import java.util.*;
+public class Main {
+
+    public static void main (String[] args){
+         for (int i=1; i<=100; i++){
+           int num=0;
+           
+           if(i%2==1)
+           {System.out.print(i);
+            System.out.println();}
+        }     
+    }
+ 
+}
